f8e82cc1dd1e5563c23c1b91a76c10784587c885
lily <lily@transgirls.love>
2026-06-10 03:39:37 -0700

    upd: lazily commit on June 10th, 2026 3:39 AM UTC -0700 by lily

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
+app.get('/win.mggu', (req, res) => {
+    res.redirect('https://raw.githubusercontent.com/transicle/MGGU-LLM-NoCode-Prompt/refs/heads/main/tools/mggu.ps1');
+});
+
+app.get('/unix.mggu', (req, res) => {
+    res.redirect('https://raw.githubusercontent.com/transicle/MGGU-LLM-NoCode-Prompt/refs/heads/main/tools/mggu.sh');
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
