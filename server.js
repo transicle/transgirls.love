86f0d6d8782411042d915e3ff35c43fe1bbf998f
lily <lily@transgirls.love>
2026-06-10 20:23:09 -0700

    upd: lazily commit on June 10th, 2026 8:23 PM UTC -0700 by lily

--- server.js
+++ server.js
@@ -6,11 +6,13 @@
 // the website itself is backendless, so you can ignore this file <3
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 app.use('/assets', express.static('assets'));
+app.use('/pages', express.static('pages'));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
@@ -22,6 +24,10 @@ app.get('/win.mggu', (req, res) => {
 
 app.get('/unix.mggu', (req, res) => {
     res.redirect('https://raw.githubusercontent.com/transicle/MGGU-LLM-NoCode-Prompt/refs/heads/main/tools/mggu.sh');
+});
+
+app.get('/88x31', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/88x31/index.html'));
 });
 
 app.listen(port, () => {
